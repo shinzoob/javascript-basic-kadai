31551c1002275cb41b8055b65dbc7ea7a0cc948a
DESKTOP-8QKQLFG\Shingo <shingo.b@hotmail.co.jp>
2024-06-30 22:26:16 +0900

    mdn_月の表示の修正

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,6 +1,6 @@
 const time = new Date;
 const year = time.getFullYear();
-const month = time.getMonth();
+const month = time.getMonth() + 1;
 const date = time.getDate();
 const output = year + "年" + month + "月" + date + "日";
 console.log(output);
